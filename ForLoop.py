0a80e16f07dae39fe424e4ccfee69319564b4a64
Jie Ming <91531539+JieMing71902@users.noreply.github.com>
2021-10-30 17:07:34 +0800

    For Loop for Python

---
+++ ForLoop.py
@@ -0,0 +1,12 @@
+for num1 in range (10): #not including 10
+    print(num1)
+
+for num2 in range (2,10): #not including 10
+    print(num2)
+
+cars=["perodua", "proton","honda"]
+for num3 in cars:
+    print(num3)
+
+for num4 in range (2,10,2): #not including 10
+    print(num4)
---
+++ IfElse.py
@@ -0,0 +1,9 @@
+
+x=100
+y=50
+if y>x:
+    print("y is greater than x")
+elif y==x:
+    print("x and y are equal")
+else:
+    print("x is greater than y")
